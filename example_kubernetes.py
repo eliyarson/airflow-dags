c7cd07c6ca95bc6d587cedc7ccaa880e4004aa69
Eli <eli_tgt@hotmail.com>
2021-05-16 18:25:03 -0300

    Change task namespace

--- example_kubernetes.py
+++ example_kubernetes.py
@@ -25,7 +25,7 @@ dag = DAG(
 start = DummyOperator(task_id="run_this_first", dag=dag)
 
 passing = KubernetesPodOperator(
-    namespace="default",
+    namespace="dataserver",
     image="Python:3.6",
     cmds=["Python", "-c"],
     arguments=["print('hello world')"],
