95eb5e0c5d402d39b9b447fd02342dbbc0e28ada
Eli <eli_tgt@hotmail.com>
2021-05-18 23:06:13 -0300

    Change arguments

--- example_kubernetes.py
+++ example_kubernetes.py
@@ -101,7 +101,7 @@ with DAG(
         namespace='airflow',
         image="gcr.io/meliuz-poc-data-lake/dbt:latest",
         cmds=["bash","-cx"],
-        arguments=["dbt run --profiles-dir /dbt --target dev-airflow"],
+        arguments=["dbt run --target dev-airflow"],
         labels={"foo": "bar"},
         image_pull_secrets='registrykey',
         image_pull_policy='Always',
